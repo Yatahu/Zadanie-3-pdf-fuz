434110ad9efc0652cee5b485e1db817bcceb9d6b
Wojtek Szyjka <wojtus0708@gmail.com>
2020-11-27 19:38:59 +0100

    Initial Commit

---
+++ Zadanie-3-pdf-fuz/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Zadanie-3-pdf-fuz
+//
+//  Created by Wojciech Szyjka on 27/11/2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
